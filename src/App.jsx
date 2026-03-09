0727211155b744e16a77447ec451d8706b71f165
sourovroy999 <sourovroy3050@gmail.com>
2026-03-09 19:58:55 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 
+import gsap from 'gsap'
 import './App.css'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import Navbar from './components/Navbar'
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
 
 function App() {
 
 
 
   return (
-  <main>
-    
-  </main>
+
+    <main>
+
+      <Navbar/>
+
+    </main>
+
+  // <main>
+
+  // </main>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { useGSAP } from '@gsap/react';
+import React from 'react';
+import { navLinks } from '../../constants';
+
+const Navbar = () => {
+
+    useGSAP(()=>{
+
+    })
+
+    
+    return (
+        <nav className='fixed z-50 w-full'>
+            <div className='flex md:flex-row flex-col md:justify-between items-center gap-5 py-5 lg:px-0 px-5 container mx-auto'>
+                <a href="http://" className='flex items-center gap-2'>
+                <img src="/public/images/logo.png" alt="" srcset="" />
+                <p>velvet pour</p>
+                </a>
+
+                <ul className='flex-center flex lg:gap-12 gap-7'>
+                   
+              {navLinks.map((link) => (
+			<li key={link.id}>
+			 <a href={`#${link.id}`}>{link.title}</a>
+			</li>
+		 ))}
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
