5c0331f823fa3dceb7f0bc4808479632db20579b
sourovroy999 <sourovroy3050@gmail.com>
2026-03-16 16:54:55 +0600

    menu details added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { allCocktails } from '../../constants';
 
 const Menu = () => {
+
+    const contentRef = useRef()
 
     const [currentIndex, setCurrentIndex] = useState(0)
 
@@ -16,19 +18,19 @@ const Menu = () => {
 
     }
 
-    const getCocktailAt=(indexoffset)=>{
-        return allCocktails[(currentIndex + indexoffset +totalCocktails) % totalCocktails]
+    const getCocktailAt = (indexoffset) => {
+        return allCocktails[(currentIndex + indexoffset + totalCocktails) % totalCocktails]
     }
 
-    const currentCocktail=getCocktailAt(0)
-    const prevCocktail=getCocktailAt(-1)
-    const nextCocktail=getCocktailAt(1)
+    const currentCocktail = getCocktailAt(0)
+    const prevCocktail = getCocktailAt(-1)
+    const nextCocktail = getCocktailAt(1)
     return (
         <section id='menu' aria-labelledby='menu-heading' className='relative w-full md:mt-40 mt-0 2xl:px-0 px-5 py-20 radial-gradient'>
 
             <h2 id='menu-heading' className='sr-only'>Cocktail Menu</h2>
 
-            <nav className='grid md:grid-cols-4 grid-cols-2 md:gap-20 gap-10 sm:mb-32 mb-20 relative z-10 md:max-w-6xl md:mx-auto' aria-label='Cocktail-Navigation'>
+            <nav className='cocktail-tabs grid md:grid-cols-4 grid-cols-2 md:gap-20 gap-10 sm:mb-32 mb-20 relative z-10 md:max-w-6xl md:mx-auto' aria-label='Cocktail-Navigation'>
                 {
                     allCocktails.map((cocktail, index) => {
                         const isActive = index === currentIndex;
@@ -47,28 +49,45 @@ const Menu = () => {
 
             </nav>
 
-            <div className='flex flex-col content justify-between items-center container mx-auto relative'>
+            <div className='flex content flex-col content justify-between items-center container mx-auto relative'>
 
                 <div className='arrows flex items-center justify-between w-full absolute'>
 
-                    <button  className='text-left  text-white hover:text-yellow cursor-pointer transition-colors max-w-36' onClick={()=> goToSlide(currentIndex-1)}>
-                        <span className='text-3xl font-modern-negra leading-none hidden md:block'>{prevCocktail.name }</span>
+                    <button className='text-left  text-white hover:text-yellow cursor-pointer transition-colors max-w-36' onClick={() => goToSlide(currentIndex - 1)}>
+                        <span className='text-3xl font-modern-negra leading-none hidden md:block'>{prevCocktail.name}</span>
                         <img src="/images/right-arrow.png" alt="" />
 
                     </button>
 
-                    <button className='text-left  text-white hover:text-yellow cursor-pointer transition-colors max-w-36'  onClick={()=> goToSlide(currentIndex+1)}>
-                        <span className='text-3xl font-modern-negra leading-none hidden md:block'>{nextCocktail.name }</span>
+                    <button className='text-left  text-white hover:text-yellow cursor-pointer transition-colors max-w-36' onClick={() => goToSlide(currentIndex + 1)}>
+                        <span className='text-3xl font-modern-negra leading-none hidden md:block'>{nextCocktail.name}</span>
                         <img src="/images/left-arrow.png" alt="" />
 
                     </button>
 
                 </div>
 
-                <div className='cocktail'>
-                    <img src={currentCocktail.image} alt="" className='object-contain'/> 
+                <div className='cocktail flex justify-center items-center mt-10'>
+                    <img src={currentCocktail.image} alt="" className='object-contain h-[60vh]' />
 
                 </div>
+
+                <div className="recipe flex max-md:flex-col gap-10 md:items-center justify-between w-full lg:absolute bottom-0">
+                    <div ref={contentRef} className='info space-y-4 lg:translate-y-20'>
+                        <p>Recepe for:</p>
+                        <p className='font-modern-negra md:text-6xl text-3xl text-yellow max-w-40' id='title'>{currentCocktail.name}</p>
+
+                    </div>
+
+                    <div className="details  space-y-5 md:max-w-md text-left">
+                        <h2 className='md:text-5xl text-3xl font-serif'>{currentCocktail.title}</h2>
+                        <p className=' md:text-lg pe-5'>{currentCocktail.description}</p>
+                    </div>
+                </div>
+
+
+
+
 
 
 
